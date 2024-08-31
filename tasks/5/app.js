3447c28e515ba5e7ec36e6e941918c804e6b1a28
hossein <hosseinghiyasi2020@gmail.com>
2024-08-31 14:36:47 +0330

    final

--- tasks/5/app.js
+++ tasks/5/app.js
@@ -1,27 +1,4 @@
-// let MyObject = 
-//   {
-//   a: 1,
-//   b: {
-//     c:2,
-//     d: {
-//       e: 3
-//     }
-//   }
-//   }
 
-// function calcNumbers(obj) {
-//   let sum = 0;
-//   for (let key in obj) {
-//     if (typeof obj[key] === 'number') {
-//       sum+=obj[key];
-//     } else if(typeof obj[key] === 'object') {
-//       sum += calcNumbers(obj[key]);
-//     }
-//   }
-//   return sum;
-// }
-
-// console.log(calcNumbers(MyObject));
 
 
 const MyObject = {
